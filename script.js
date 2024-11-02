912d23c146a64647d52682cfa4eb345489b1afc8
superjhay29 <105788845+superjhay29@users.noreply.github.com>
2024-11-01 21:15:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const adminPassword = "jhayjhayservices.com"; // Admin password
+let userCredits = 850; // Initial credits for the user
+
+function login() {
+    const password = document.getElementById('adminPassword').value;
+    const loginMessage = document.getElementById('loginMessage');
+
+    if (password === adminPassword) {
+        document.querySelector('.login').style.display = 'none';
+        document.getElementById('downloadContainer').style.display = 'block';
+        updateCreditsDisplay();
+    } else {
+        loginMessage.textContent = "Invalid Admin Password.";
+    }
+}
+
+function updateCreditsDisplay() {
+    document.getElementById('creditDisplay').textContent = `You have ${userCredits} credits.`;
+}
+
+function subtractCredits() {
+    if (userCredits >= 10) {
+        userCredits -= 10;
+        updateCreditsDisplay();
+        document.getElementById('message').textContent = "Download initiated.";
+    } else {
+        document.getElementById('message').textContent = "Insufficient credits for this download.";
+    }
+}
